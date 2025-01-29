749b3ac6e40352af0f23d8fff328273d53fee1ef
simunpat <spt@lunnar.fo>
2025-01-29 18:07:09 +0100

    tilføjede menu og program.cs funktionalitet

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading.Tasks;
+
+/// <summary>
+/// Hovedprogrammet for Sydvest-Bo administrationssystem.
+/// Dette er indgangspunktet for applikationen.
+/// </summary>
+class Program
+{
+    /// <summary>
+    /// Hovedmetoden der starter applikationen.
+    /// Initialiserer menusystemet og håndterer overordnede fejl.
+    /// </summary>
+    static async Task Main(string[] args)
+    {
+        try
+        {
+            Console.Title = "Sydvest-Bo Administrationssystem";
+            
+            // Sæt console encoding til at håndtere danske tegn
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.InputEncoding = System.Text.Encoding.UTF8;
+
+            // Vis velkomstbesked
+            Console.WriteLine("Velkommen til Sydvest-Bo Administrationssystem");
+            Console.WriteLine("Indlæser...");
+            Console.WriteLine();
+
+            // Test database forbindelse
+            try
+            {
+                // TODO: Implementer database connection test
+                await Task.Delay(1000); // Simulerer database check
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Fejl ved forbindelse til database:");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("\nTryk på en tast for at afslutte...");
+                Console.ReadKey();
+                return;
+            }
+
+            // Start menusystemet
+            var menu = new Menu();
+            await menu.VisHovedMenu();
+        }
+        catch (Exception ex)
+        {
+            // Håndter uventede fejl
+            Console.Clear();
+            Console.WriteLine("Der opstod en uventet fejl i programmet:");
+            Console.WriteLine(ex.Message);
+            Console.WriteLine("\nDetaljer:");
+            Console.WriteLine(ex.ToString());
+            Console.WriteLine("\nProgrammet vil nu afslutte. Tryk på en tast...");
+            Console.ReadKey();
+        }
+    }
+}
